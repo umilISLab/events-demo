0dbe6b78c1ed6b2c072e50fa0ba7016dace88270
davidelamorte <lamortedavide@gmail.com>
2024-02-07 08:55:45 +0100

    working docker compose

--- backend/db/conn.mjs
+++ backend/db/conn.mjs
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
+import data from "./demo_full_data.json" assert { type: "json" };
 
-const connectionString = "mongodb://localhost:27017/";
+const connectionString = "mongodb://mongo:27017/events-demo";
 const client = new MongoClient(connectionString);
 
 let conn;
@@ -11,5 +12,69 @@ try {
 }
 
 let db = conn.db("events-demo");
+
+// import data in db
+
+db.collection("events").drop();
+
+const eventsCollection = db.collection("events");
+
+data.forEach((el, i) => {
+  let events = [];
+  let roles = [];
+
+  if (el.title.spans) {
+    Object.keys(el.title.spans).forEach((span) => {
+      const splitted = span.split("*");
+      if (splitted.length === 1) {
+        events.push({
+          label: span,
+          location: "title",
+        });
+      } else {
+        roles.push({
+          label: splitted[0],
+          value: el.title.spans[span],
+          location: "title",
+        });
+      }
+    });
+  }
+
+  if (el.body.length) {
+    el.body.forEach((sentence, y) => {
+      if (sentence.spans) {
+        Object.keys(sentence.spans).forEach((span) => {
+          const splitted = span.split("*");
+          if (splitted.length === 1) {
+            events.push({
+              label: span,
+              location: "body",
+            });
+          } else {
+            roles.push({
+              label: splitted[0],
+              value: el.body[y].spans[span],
+              location: "body",
+            });
+          }
+        });
+      }
+    });
+  }
+
+  eventsCollection.insertOne({
+    id: el.id,
+    title: el.title,
+    body: el.body,
+    date: new Date(el.date),
+    events,
+    roles,
+  });
+
+  console.log("insert " + i);
+});
+
+console.log("end");
 
 export default db;
