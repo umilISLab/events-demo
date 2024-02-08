3a8a7e6d0b9625731253e6969ab395c027071466
davidelamorte <lamortedavide@gmail.com>
2024-02-08 15:35:58 +0100

    import script change

--- backend/db/conn.mjs
+++ backend/db/conn.mjs
@@ -1,7 +1,9 @@
 import { MongoClient } from "mongodb";
 import data from "./demo_full_data.json" assert { type: "json" };
 
-const connectionString = "mongodb://mongo:27017/events-demo";
+// const connectionString = "mongodb://mongo:27017/events-demo";
+const connectionString = "mongodb://localhost:27017/events-demo";
+
 const client = new MongoClient(connectionString);
 
 let conn;
@@ -29,12 +31,16 @@ data.forEach((el, i) => {
       if (splitted.length === 1) {
         events.push({
           label: span,
+          value: el.title.spans[span],
+          text: el.title.spans[span].join(" "),
           location: "title",
         });
       } else {
         roles.push({
           label: splitted[0],
+          event: splitted[1],
           value: el.title.spans[span],
+          text: el.title.spans[span].join(" "),
           location: "title",
         });
       }
@@ -49,12 +55,16 @@ data.forEach((el, i) => {
           if (splitted.length === 1) {
             events.push({
               label: span,
+              value: el.body[y].spans[span],
+              text: el.body[y].spans[span].join(" "),
               location: "body",
             });
           } else {
             roles.push({
               label: splitted[0],
+              event: splitted[1],
               value: el.body[y].spans[span],
+              text: el.body[y].spans[span].join(" "),
               location: "body",
             });
           }
